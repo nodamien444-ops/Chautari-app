9dc41e892e9a78d397718c29bbc24be880f9775b
Damien Nova <nodamien444@gmail.com>
2025-09-06 18:19:25 +0545

    Add in-memory chat feature

--- server.js
+++ server.js
@@ -3,12 +3,34 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the current folder
+// Store messages in memory
+let messages = [];
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Serve static files
 app.use(express.static(path.join(__dirname)));
 
 // Route for homepage
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Get all messages
+app.get('/messages', (req, res) => {
+  res.json(messages);
+});
+
+// Post a new message
+app.post('/messages', (req, res) => {
+  const { text } = req.body;
+  if (text) {
+    messages.push(text);
+    res.status(201).json({ success: true, messages });
+  } else {
+    res.status(400).json({ success: false, message: 'No text provided' });
+  }
 });
 
 app.listen(PORT, () => {
